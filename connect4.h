1d31c8985eef1ee986ba527326d0aefc1e71783e
Nick McClorey <mcclorns@mail.uc.edu>
2018-05-03 22:11:35 -0400

    set up class for board. time to implement

--- connect4.h
+++ connect4.h
@@ -4,7 +4,7 @@
 using namespace std;
 
 // from game_manager
-int changeTurn(const int &currentTurn);
+int changeTurn();
 int checkWin(const vector<vector<int> > &board, const int &row, const int &col, const int &turn);
 int dropPiece(vector <vector <int> > &board, const int &dropLocation, const int &turn);
 void displayBoard(const vector <vector<int> > &board);
@@ -18,6 +18,33 @@ bool userCanWin(const vector<vector<int> > &board, int turn);
 int showCombinations(const vector<vector<int> > board);
 void runthrough(const vector<vector<int> > &board, const vector<int> &moves);
 //bool isGoodMove(vector<vector<int> >board, int turn, const int &length, vector<int> moves);
+
+
+class connect4Board {
+private:
+
+	int board[7][7];
+	int turn;
+	bool gameIsOver;
+	int lastX; // x position of last piece dropped
+	int lastY; // y position of last piece dropped
+
+
+public:
+	enum teams { empty, player, computer };
+	int checkWin();
+
+	int getTurn();
+	void setTurn(int team);
+	void changeTurn();
+	int at(int x, int y);
+	bool isFull(int column);
+	void dropPiece(int column);
+	void displayBoard();
+	void setAt(int x, int y, int team);
+	bool isValidIndex(int, int);
+
+};
 
 
 #endif
--- game_manager.cpp
+++ game_manager.cpp
@@ -9,8 +9,184 @@
 using namespace std;
 
 
+
+
+
+int connect4Board::getTurn() {
+	return this->turn;
+}
+
+void connect4Board::setTurn(int team) {
+	this->turn = team;
+}
+
+void connect4Board::changeTurn() {
+	this->turn = (this->turn == player) ? computer : player;
+}
+
+
 int changeTurn(const int &currentTurn) {
 	return (currentTurn == 1) ? 2 : 1;
+}
+
+bool connect4Board::isFull(int column) {
+	if (this->at(column, 6) != empty) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool connect4Board::isValidIndex(int x, int y) {
+	if ((x > 6) || (y > 6) || (x < 0) || (y < 0)) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+int connect4Board::at(int x, int y) {
+	if (!this->isValidIndex(x, y)) {
+		cout << x << "," << y << " is an invalid index" << endl;
+		exit(0);
+	}
+	return board[x][y];
+}
+
+void connect4Board::setAt(int x, int y, int team) {
+	if (!this->isValidIndex(x, y)) {
+		cout << x << "," << y << " is an invalid index" << endl;
+		exit(0);
+	} else if ((team > 2) || (team < 0)) {
+		cout << team << " is not a valid team" << endl;
+		exit(0);
+	}
+
+	board[x][y] == team;
+
+}
+
+int connect4Board::checkWin() {
+	// checking to find a horizontal row
+	int streak = 0;
+	for (int k = 0; k < 7; k++) {
+
+		if (this->at(k, lastY) == turn) {
+			streak++;
+			//cout << "streak: " << streak << endl;
+		}
+		else {
+			streak = 0;
+		}
+
+		if (streak >= 4)
+			return turn;
+	}
+
+	// checkign for vertical win
+	streak = 0;
+	for (int k = 0; k < 7; k++) {
+
+		if (this->at(lastX, k) == turn) {
+			streak++;
+			//cout << "streak: " << streak << endl;
+		}
+		else {
+			streak = 0;
+		}
+
+		if (streak >= 4)
+			return turn;
+	}
+
+	// checking diagonal win
+
+	// finding edge of board
+	int r = lastY; int c = lastX;
+	while ((r < 6) && (c < 6)) {
+		r++;
+		c++;
+	}
+
+	// going from edge diagonally, checkign for streaks
+	streak = 0;
+	while ((r >= 0) && (c >= 0)) {
+		if (this->at(c, r) == turn) {
+			streak++;
+		}
+		else {
+			streak = 0;
+		}
+
+		if (streak >= 4) {
+			return turn;
+		}
+
+		r--;
+		c--;
+
+	}
+
+	// finding the edge
+	r = lastY; c = lastX;
+	while ((r > 0) && (c < 6)) {
+		r--;
+		c++;
+	}
+
+	// looking for streaks
+	streak = 0;
+	while ((r <= 6) && (c >= 0)) {
+		if (this->at(c, r) == turn) {
+			streak++;
+		}
+		else {
+			streak = 0;
+		}
+
+		if (streak >= 4) {
+			return turn;
+		}
+
+		r++;
+		c--;
+
+	}
+
+
+	return 0;
+}
+
+void connect4Board::dropPiece(int dropLocation) {
+	
+	int k = 0;
+	for (; k < 7; k++) {
+		if (this->at(dropLocation, k) == 0) {
+			this->setAt(dropLocation, k, turn);
+			return;
+		}
+	
+	}
+	
+}
+
+void connect4Board::displayBoard() {
+	cout << endl << endl << endl;
+
+	// provide column numbers
+	for (int k = 0; k < 7; k++) {
+		cout << k << " ";
+	}
+	cout << endl << endl;
+
+	// display board fromm top down
+	for (int row = 6; row >= 0; row--) {
+		for (int col = 0; col < 7; col++) {
+			cout << this->at(col, row) << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
 }
 
 // checks area around new piece to see if he won
